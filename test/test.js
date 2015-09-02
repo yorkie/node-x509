268d60499bdf0b8a2e0ac099d5218c1099b22fcf
Cyprien DIOT <cyprien.diot@pmsipilot.com>
2015-09-02 18:04:17 +0200

    adding simple test for getPemFromP12

--- test/test.js
+++ test/test.js
@@ -3,10 +3,14 @@ var x509 = require('../index'),
     path = require('path');
 
 // All cert files should read without throwing an error.
-// Simple enough test, no?
+// Simple enough test, no? 
+
 fs.readdirSync(path.join(__dirname, 'certs')).forEach(function (file) {
   console.log("File: %s", file);
   console.log(x509.parseCert(path.join(__dirname, 'certs', file)));
   // x509.parseCert(path.join(__dirname, 'certs', file));
   console.log();
 });
+
+
+console.log(x509.parseCert(x509.getPemFromP12("test/p12/cert.p12", "password")));
